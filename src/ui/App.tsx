4f755c5383a0fe0e0ebae5c1c07afbdae2fe0a44
Im-Stivik <mogil.sharon@gmail.com>
2025-05-03 13:18:34 +0300

    done

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,29 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import MainPannel from './layout/MainPannel'
+
+import SidePannel from './layout/SidePannel'
+import TopPannel from './layout/TopPannel'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', margin: 0, top: 0 }}>
+        <div>
+          <TopPannel />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '100%' }}>
+          <SidePannel />
+          <div style={{ flexDirection: 'column', width: '100%' }}>
+            <MainPannel />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/ui/layout/TopPannel.tsx
@@ -0,0 +1,14 @@
+function TopPannel() {
+  return (
+    <>
+      <div className="pannel">
+        <ul>
+          <li>Top Panel</li>
+
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default TopPannel
